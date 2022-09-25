39fba1980c77e8e68183268251b27b2aed097018
Ali <ali.chouchene@insat.univ-carthage.tn>
2022-09-25 20:17:37 +0100

    adding .properties file

---
+++ src/Memory_dumper.ino
@@ -0,0 +1,15 @@
+#include "MemoryDumper.h"
+
+
+
+void setup() {
+  StartDumping(9600);    // start dumping by enabling the serial output
+
+}
+
+void loop() {
+  Dump("PORTB");   // dump all registers contents
+  StopDumping();        // Stop dumping by disabling the serial output
+  while(1);             // wait for ever
+
+}
